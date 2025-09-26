f731533077d66cd92879e17041bce573fc46f967
Zhadiger <98839309+Zhadi-1-s@users.noreply.github.com>
2025-09-26 21:17:18 +0500

    создал футер и доработал home page

--- reTech/src/app/app.component.ts
+++ reTech/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { app } from '../../server';
 import { NavbarComponent } from './common/components/navbar/navbar.component';
 import { CommonModule } from '@angular/common';
 import { TopbarComponent } from './common/components/topbar/topbar.component';
+import { FooterComponent } from './common/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent,CommonModule,TopbarComponent],
+  imports: [RouterOutlet, NavbarComponent,CommonModule,TopbarComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
